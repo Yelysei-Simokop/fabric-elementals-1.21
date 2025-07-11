53c3166d57cab0fb797406d00540cb0f63ceaa27
Yelysei-Simokop <yelyseisimokop@gmail.com>
2025-07-11 23:24:21 +0200

    Initial commit

---
+++ src/main/java/com/name/elementals/ElementalsClient.java
@@ -0,0 +1,10 @@
+package com.name.elementals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElementalsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
