8c193206c3e40fdbc60a226f0c5c4bdf31c3d681
Yelysei-Simokop <yelyseisimokop@gmail.com>
2025-07-20 01:25:36 +0200

    Add elemental powders.
    Add Soul Fire Powder.
    Add Wind Powder.
    Add Water Powder.
    Add Earth Powder.
    Add Floral Powder.
    Add Void Powder.
    Add Sculk Powder.
    Add Lightning Powder.
    Add Frost Powder.
    
    Change Fire Powder texture.

--- src/main/java/com/name/elementals/item/ModItems.java
+++ src/main/java/com/name/elementals/item/ModItems.java
@@ -10,14 +10,26 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     //technically adds the item to minecraft
+
+    //Rods
     public static final Item SOUL_BLAZE_ROD = registerItem("soul_blaze_rod", new Item(new Item.Settings()));
+    public static final Item TORRENT_ROD = registerItem("torrent_rod", new Item(new Item.Settings()));
+    public static final Item CLOD_ROD = registerItem("clod_rod", new Item(new Item.Settings()));
     public static final Item BLOOM_ROD = registerItem("bloom_rod", new Item(new Item.Settings()));
     public static final Item NULL_ROD = registerItem("null_rod", new Item(new Item.Settings()));
-    public static final Item TORRENT_ROD = registerItem("torrent_rod", new Item(new Item.Settings()));
     public static final Item ECHO_ROD = registerItem("echo_rod", new Item(new Item.Settings()));
     public static final Item BLITZ_ROD = registerItem("blitz_rod", new Item(new Item.Settings()));
-    public static final Item CLOD_ROD = registerItem("clod_rod", new Item(new Item.Settings()));
     public static final Item FREEZE_ROD = registerItem("freeze_rod", new Item(new Item.Settings()));
+    //Powders
+    public static final Item SOUL_FIRE_POWDER = registerItem("soul_fire_powder", new Item(new Item.Settings()));
+    public static final Item WIND_POWDER = registerItem("wind_powder", new Item(new Item.Settings()));
+    public static final Item WATER_POWDER = registerItem("water_powder", new Item(new Item.Settings()));
+    public static final Item EARTH_POWDER = registerItem("earth_powder", new Item(new Item.Settings()));
+    public static final Item FLORAL_POWDER = registerItem("floral_powder", new Item(new Item.Settings()));
+    public static final Item VOID_POWDER = registerItem("void_powder", new Item(new Item.Settings()));
+    public static final Item SCULK_POWDER = registerItem("sculk_powder", new Item(new Item.Settings()));
+    public static final Item LIGHTNING_POWDER = registerItem("lightning_powder", new Item(new Item.Settings()));
+    public static final Item FROST_POWDER = registerItem("frost_powder", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Elementals.MOD_ID, name), item);
@@ -28,14 +40,25 @@ public class ModItems {
 
         //Adds the item to the INGERDIENTS creative tab
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+                //Rods
                 entries.add(SOUL_BLAZE_ROD);
+                entries.add(TORRENT_ROD);
+                entries.add(CLOD_ROD);
                 entries.add(BLOOM_ROD);
                 entries.add(NULL_ROD);
-                entries.add(TORRENT_ROD);
                 entries.add(ECHO_ROD);
                 entries.add(BLITZ_ROD);
-                entries.add(CLOD_ROD);
                 entries.add(FREEZE_ROD);
+                //Powders
+                entries.add(SOUL_FIRE_POWDER);
+                entries.add(WIND_POWDER);
+                entries.add(WATER_POWDER);
+                entries.add(EARTH_POWDER);
+                entries.add(FLORAL_POWDER);
+                entries.add(VOID_POWDER);
+                entries.add(SCULK_POWDER);
+                entries.add(LIGHTNING_POWDER);
+                entries.add(FROST_POWDER);
         });
     }
 }
